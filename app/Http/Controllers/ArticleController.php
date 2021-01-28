853157b6411c9350de5a3769d7012d9a830969bb
chesterkyles <chesterkylescolita07@gmail.com>
2021-01-28 21:04:41 +0800

    fcx

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,7 +10,7 @@ class ArticleController extends Controller
 {
     public function create()
     {
-        return view('article.create');
+        return view( 'article.create' );
     }
 
     public function index()
